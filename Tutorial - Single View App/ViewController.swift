a7ecebb860aaf62a5dac1cd26e11ea9b480a037d
Simon <simonaverhoeven@gmail.com>
2020-12-22 21:38:02 +0800

    finished tutorial

--- Tutorial - Single View App/ViewController.swift
+++ Tutorial - Single View App/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -34,8 +37,20 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+       
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if #available(iOS 13.0, *) {
+            if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+                vc.selectedImage = pictures[indexPath.row]
+                navigationController?.pushViewController(vc, animated: true)
+            }
+        } else {
+            // Fallback on earlier versions
+        }
     }
 
 }
